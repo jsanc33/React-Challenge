d0a13241fb3f02eba699bb4bde3f4d8c438790bd
Joseph Sanchez <sanchezjoseph33@gmail.com>
2025-04-30 09:53:06 -0500

    added resume link

--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -1,28 +1,43 @@
 export default function Resume() {
+
+  const skills = [
+    "JavaScript",
+    "TypeScript",
+    "Python",
+    "Shell Scripting",
+    "CSS3",
+    "HTML5",
+    "React",
+    "Redux",
+    "Tailwind CSS",
+    "Bootstrap",
+    "Material-UI",
+    "Foundation",
+    "Node.js",
+    "Express.js",
+    "PostgreSQL",
+    "MongoDB",
+    "GraphQL",
+    "Apollo",
+    "Sequelize",
+    "Mongoose",
+    "GitHub",
+    "GitLab",
+    "Jest",
+    "GitHub Actions",
+    "Vite",
+  ];
   return (
     <div>
       <h1>Resume</h1>
-      <p>
-        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
-        Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
-        dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
-        sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum nunc,
-        sed pretium risus rutrum eget. Nam consequat, ligula in faucibus
-        vestibulum, nisi justo laoreet risus, luctus luctus mi lacus sit amet
-        libero. Class aptent taciti sociosqu ad litora torquent per conubia
-        nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
-        lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-        Donec placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum
-        in velit pellentesque vehicula sit amet at augue. Maecenas aliquam
-        bibendum congue. Pellentesque semper, lectus non ullamcorper iaculis,
-        est ligula suscipit velit, sed bibendum turpis dui in sapien.
-      </p>
+     <a href="https://docs.google.com/document/d/1k9vUFV7EVjn0LYbeZbUgjDeCZtz_SGaG/edit?usp=sharing&ouid=112861811330360594968&rtpof=true&sd=true" target="_blank" rel="noopener noreferrer"/>
       <section>
-      <h2>Skills</h2>
-      <ul>
-        <li>JavaScript</li>
-        <li>HTML</li>
-      </ul>
+        <h2>Skills</h2>
+        <ul>
+          {skills.map((skill, index) => (
+            <li key={index}>{skill}</li>
+          ))}
+        </ul>
       </section>
     </div>
   );
